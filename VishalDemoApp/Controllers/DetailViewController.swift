6f5fc4ffa899789340fae1d339dcfbee72748d7c
vishal <vishalupatel@gmail.com>
2020-02-26 11:46:41 +0530

    Added employee detail page

---
+++ VishalDemoApp/Controllers/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  VishalDemoApp
+//
+//  Created by Vishal Patel on 25/02/20.
+//  Copyright © 2020 Vishal Patel. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var employee:Employee?
+    
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblSalary: UILabel!
+    @IBOutlet weak var lblAge: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDataInView()
+    }
+    
+    func setDataInView(){
+        
+        guard let employee = employee else { return }
+        
+        self.title = employee.name
+        
+        lblName.text = employee.name
+        lblAge.text = employee.age
+        lblSalary.text = employee.salary
+    }
+    
+}
